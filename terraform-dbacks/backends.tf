8b8a750d43c1ab574eaf3dcd4be32895b2da5b9b
Rafael <campos@ciandt.com>
2024-06-19 14:58:20 -0300

    Change tf-state bucket

--- terraform-dbacks/backends.tf
+++ terraform-dbacks/backends.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "cs-tfstate-us-central1-9d634560bc30467396c52f621115bb99" #TBD
+    bucket = "dbacks-tfstate-us-central1" #TBD
     prefix = "terraform/state"
   }
 }
